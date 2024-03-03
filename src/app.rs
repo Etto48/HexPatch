580d05968c3196518f0f1f3a224876933e2cbf3f
Ettore Ricci <minecraft.etto@gmail.com>
2024-03-03 19:19:52 +0100

    now with colors

--- src/app.rs
+++ src/app.rs
@@ -1,16 +1,15 @@
 use std::{path::PathBuf, time::Duration};
 
 use crossterm::event::{self, KeyCode};
-use tui::{backend::Backend, layout::Rect, text::Text, widgets::Block};
+use tui::{backend::Backend, layout::Rect, style::{Color, Modifier, Style}, text::{Span, Spans, Text}, widgets::Block};
 
-pub struct App 
+pub struct App<'a> 
 {
     path: PathBuf,
     output: String,
-    address_view: String,
-    data: String,
-    text_view: String,
-    row_count: usize,
+    address_view: Text<'a>,
+    data: Text<'a>,
+    text_view: Text<'a>,
     scroll: u16,
     cursor: (u16, u16),
     poll_time: Duration,
@@ -21,7 +20,7 @@ pub struct App
     blocks_per_row: usize,
 }
 
-impl App
+impl <'a> App<'a>
 {
     pub fn new(file_path: PathBuf) -> Result<Self,String>
     {
@@ -32,9 +31,8 @@ impl App
             path: file_path,
             output: "^C: quit, ^S: save, ^X: save and quit".to_string(),
             address_view: Self::addresses(data.len(), block_size, blocks_per_row),
-            data: Self::vec_u8_to_hex_string(&data, block_size, blocks_per_row),
-            text_view: Self::vec_u8_to_string(&data, block_size, blocks_per_row),
-            row_count: Self::row_count(data.len(), block_size, blocks_per_row),
+            data: Self::bytes_to_styled_hex(&data, block_size, blocks_per_row),
+            text_view: Self::bytes_to_styled_text(&data, block_size, blocks_per_row),
             scroll: 0,
             cursor: (0,0),
             poll_time: Duration::from_millis(1000),
@@ -45,12 +43,117 @@ impl App
         })
     }
 
-    pub fn row_count(data_len: usize, block_size: usize, blocks_per_row: usize) -> usize
+    fn get_style_for_byte(byte: u8) -> Style
     {
-        data_len / (block_size * blocks_per_row) + 1
+        match byte
+        {
+            // null
+            0x00 => Style::default().fg(Color::DarkGray),
+            // newline
+            0x0A | 0x0C | 0x0D => Style::default().fg(Color::LightRed),
+            // whitespace
+            0x20 | 0x09 | 0x0B => Style::default().fg(Color::Rgb(244, 202, 183)),
+            // numbers
+            0x30..=0x39 => Style::default().fg(Color::Rgb(204, 152, 113)).add_modifier(Modifier::BOLD),
+            // uppercase
+            0x41..=0x5A => Style::default().fg(Color::Rgb(204, 152, 113)).add_modifier(Modifier::BOLD),
+            // lowercase
+            0x61..=0x7A => Style::default().fg(Color::Rgb(204, 152, 113)).add_modifier(Modifier::BOLD),
+            // special characters
+            0x20..=0x7E => Style::default().fg(Color::Rgb(204, 152, 113)).add_modifier(Modifier::DIM),
+            _ => Style::default()
+        }
     }
 
-    pub fn resize_if_needed(&mut self, width: u16)
+    fn bytes_to_styled_hex(bytes: &[u8], block_size: usize, blocks_per_row: usize) -> Text<'a>
+    {
+        let mut ret = Text::raw("");
+        let mut current_line = Spans::default();
+        let mut local_block = 0;
+        let mut local_byte = 0;
+        for b in bytes
+        {
+            let mut next_line = false;
+            let hex_chars = Self::u8_to_hex(*b);
+            let mut hex_string = hex_chars.iter().collect::<String>();
+            hex_string.push(' ');
+            local_byte += 1;
+            if local_byte % block_size == 0
+            {
+                local_byte = 0;
+                hex_string.push(' ');
+
+                local_block += 1;
+                if local_block % blocks_per_row == 0
+                {
+                    local_block = 0;
+                    next_line = true;
+                }
+            }
+
+            let style = Self::get_style_for_byte(*b);
+            let span = Span::styled(hex_string, style);
+            current_line.0.push(span);
+
+            if next_line
+            {
+                let new_line = std::mem::replace(&mut current_line, Spans::default());
+                ret.lines.push(new_line);
+            }
+        }
+        if current_line.0.len() > 0
+        {
+            ret.lines.push(current_line);
+        }
+
+        ret
+    }
+
+    fn bytes_to_styled_text(bytes: &'_[u8], block_size: usize, blocks_per_row: usize) -> Text<'a>
+    {
+        let mut ret = Text::raw("");
+        let mut current_line = Spans::default();
+        let mut local_block = 0;
+        let mut local_byte = 0;
+        for b in bytes
+        {
+            let mut next_line = false;
+            let char = Self::u8_to_char(*b);
+            let mut char_string = char.to_string();
+            char_string.push(' ');
+            local_byte += 1;
+            if local_byte % block_size == 0
+            {
+                local_byte = 0;
+                char_string.push(' ');
+
+                local_block += 1;
+                if local_block % blocks_per_row == 0
+                {
+                    local_block = 0;
+                    next_line = true;
+                }
+            }
+
+            let style = Self::get_style_for_byte(*b);
+            let span = Span::styled(char_string, style);
+            current_line.0.push(span);
+
+            if next_line
+            {
+                let new_line = std::mem::replace(&mut current_line, Spans::default());
+                ret.lines.push(new_line);
+            }
+        }
+        if current_line.0.len() > 0
+        {
+            ret.lines.push(current_line);
+        }
+
+        ret
+    }
+
+    fn resize_if_needed(&mut self, width: u16)
     {
         let blocks_per_row = self.calc_blocks_per_row(width);
         if self.blocks_per_row != blocks_per_row
@@ -59,17 +162,16 @@ impl App
         }
     }
 
-    pub fn resize(&mut self, blocks_per_row: usize)
+    fn resize(&mut self, blocks_per_row: usize)
     {
         self.blocks_per_row = blocks_per_row;
         let bin_data = self.data_to_vec_u8();
         self.address_view = Self::addresses(bin_data.len(), self.block_size, self.blocks_per_row);
-        self.data = Self::vec_u8_to_hex_string(&bin_data, self.block_size, self.blocks_per_row);
-        self.text_view = Self::vec_u8_to_string(&bin_data, self.block_size, self.blocks_per_row);
-        self.row_count = Self::row_count(bin_data.len(), self.block_size, self.blocks_per_row);
+        self.data = Self::bytes_to_styled_hex(&bin_data, self.block_size, self.blocks_per_row);
+        self.text_view = Self::bytes_to_styled_text(&bin_data, self.block_size, self.blocks_per_row);
     }
 
-    pub fn calc_blocks_per_row(&self, width: u16) -> usize
+    fn calc_blocks_per_row(&self, width: u16) -> usize
     {
         let block_characters_hex = self.block_size * 3 + 1;
         let block_characters_text = self.block_size * 2 + 1;
@@ -79,7 +181,7 @@ impl App
         blocks_per_row as usize
     }
 
-    pub fn u8_to_hex(input: u8) -> [char; 2]
+    fn u8_to_hex(input: u8) -> [char; 2]
     {
         let symbols = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
         let low = input & 0x0f;
@@ -87,87 +189,30 @@ impl App
         [symbols[high as usize], symbols[low as usize]]
     }
 
-    pub fn u8_to_char(input: u8) -> char
+    fn u8_to_char(input: u8) -> char
     {
-        if input >= 32 && input <= 126
+        match input
         {
-            input as char
-        }
-        else
-        {
-            '.'
+            0x20..=0x7E => input as char,
+            0x0A => '⤶',
+            _ => '.'
         }
     }
 
-    pub fn vec_u8_to_hex_string(input: &[u8], block_size: usize, blocks_per_row: usize) -> String
+    fn addresses(size: usize, block_size: usize, blocks_per_row: usize) -> Text<'a>
     {
-        let mut result = String::new();
-
-        let mut block_counter = 0;
-        let mut i = 0;
-        for byte in input
-        {
-            let hex = App::u8_to_hex(*byte);
-            result.push(hex[0]);
-            result.push(hex[1]);
-            result.push(' ');
-            i += 1;
-            if i % block_size == 0
-            {
-                i = 0;
-                result.push(' ');
-                block_counter += 1;
-                if block_counter == blocks_per_row
-                {
-                    block_counter = 0;
-                    result.push('\n');
-                }
-                
-            }
-        }
-        result
-    }
-
-    pub fn vec_u8_to_string(input: &[u8], block_size: usize, blocks_per_row: usize) -> String
-    {
-        let mut result = String::new();
-
-        let mut block_counter = 0;
-        let mut i = 0;
-        for byte in input
-        {
-            let char = App::u8_to_char(*byte);
-            result.push(char);
-            result.push(' ');
-            i += 1;
-            if i % block_size == 0
-            {
-                i = 0;
-                result.push(' ');
-                block_counter += 1;
-                if block_counter == blocks_per_row
-                {
-                    block_counter = 0;
-                    result.push('\n');
-                }
-                
-            }
-        }
-        result
-    }
-
-    pub fn addresses(size: usize, block_size: usize, blocks_per_row: usize) -> String
-    {
-        let mut result = String::with_capacity(size/(block_size * blocks_per_row) + 1);
+        let mut result = Text::raw("");
 
         for i in 0..=size/(block_size * blocks_per_row)
         {
-            result.push_str(format!("{:16X}\n", i * block_size * blocks_per_row).as_str());
+            let mut spans = Spans::default();
+            spans.0.push(Span::styled(format!("{:16X}", i * block_size * blocks_per_row), Style::default().fg(Color::DarkGray)));
+            result.lines.push(spans);
         }
         result
     }
 
-    pub fn move_cursor(&mut self, dx: i16, dy: i16)
+    fn move_cursor(&mut self, dx: i16, dy: i16)
     {
         let (x, y) = self.cursor;
         let mut x = x as i16 + dx;
@@ -185,17 +230,17 @@ impl App
         
         if x < 0
         {
-            x = self.block_size as i16 * 3 * self.blocks_per_row as i16 - 1;
+            x = (self.block_size * 3 * self.blocks_per_row + self.blocks_per_row) as i16 - 3;
             y -= 1;
         }
-        else if x >= (self.block_size * 3 * self.blocks_per_row) as i16
+        else if x > (self.block_size * 3 * self.blocks_per_row + self.blocks_per_row) as i16 - 3
         {
             x = 0;
             y += 1;
         }
-        if y >= (self.row_count as i16 - 1)
+        if y >= (self.data.lines.len() as i16 - 1)
         {
-            y = self.row_count as i16 - 1;
+            y = self.data.lines.len() as i16 - 1;
         }
         if y < 0
         {
@@ -208,7 +253,7 @@ impl App
         else if y >= (self.screen_size.1 - 3) as i16
         {
             y = self.screen_size.1 as i16 - 4;
-            if self.scroll < self.row_count as u16 - (self.screen_size.1 - 3)
+            if self.scroll < self.data.lines.len() as u16 - (self.screen_size.1 - 3)
             {
                 self.scroll += 1;
             }
@@ -218,7 +263,7 @@ impl App
 
     }
 
-    pub fn edit_data(&mut self, mut value: char)
+    fn edit_data(&mut self, mut value: char)
     {
         value = value.to_uppercase().next().unwrap(); 
 
@@ -227,51 +272,50 @@ impl App
             let local_x = self.cursor.0 % (self.block_size as u16 * 3 + 1);
             let local_byte_index = local_x / 3;
             let block_index = self.cursor.0 / (self.block_size as u16 * 3 + 1) + (self.scroll + self.cursor.1) * self.blocks_per_row as u16;
+            let local_block_index = block_index % self.blocks_per_row as u16;
             let line_index = block_index / self.blocks_per_row as u16;
-            let byte_index = ((block_index * self.block_size as u16 + local_byte_index) * 3 + block_index + line_index) as usize;
+            let line_byte_index = local_byte_index + self.block_size as u16 * local_block_index;
 
-            let byte_char_index = byte_index + (local_x % 3) as usize;
+            let mut old_str = self.data.lines[line_index as usize].0[line_byte_index as usize].content.to_string();
 
-            if byte_char_index >= self.data.len()
-            {
-                return;
+            unsafe {
+                old_str.as_bytes_mut()[(local_x % 3) as usize] = value as u8;
             }
+            
+            let hex = old_str.chars().filter(|c| c.is_whitespace() == false).collect::<String>();
 
-            self.data.replace_range(byte_char_index..byte_char_index+1, value.to_string().as_str());
-
-            let byte = u8::from_str_radix(&self.data[byte_index..byte_index+2], 16).unwrap();
-
-            let text_index = ((block_index * self.block_size as u16 + local_byte_index) * 2 + block_index + line_index) as usize;
+            let byte = u8::from_str_radix(&hex, 16).unwrap();
+            let style = Self::get_style_for_byte(byte);
+            self.data.lines[line_index as usize].0[line_byte_index as usize] = Span::styled(old_str, style);
             
             let text = App::u8_to_char(byte);
-            self.text_view.replace_range(text_index..text_index+1, text.to_string().as_str());
+            let old_str = self.text_view.lines[line_index as usize].0[line_byte_index as usize].content.to_string();
+            let text_iterator = old_str.chars().filter(|c| c.is_whitespace());
+            let mut new_str = text.to_string();
+            new_str.extend(text_iterator);
+
+            self.text_view.lines[line_index as usize].0[line_byte_index as usize] = Span::styled(new_str, style);
         }
     }
 
-    pub fn data_to_vec_u8(&self) -> Vec<u8>
+    fn data_to_vec_u8(&self) -> Vec<u8>
     {
-        let mut char_index = 0;
-        let mut output = Vec::with_capacity(self.data.len() / 2);
-        if self.data.len() != 0
+        
+        let mut output = Vec::with_capacity(self.data.lines.len() * self.block_size * self.blocks_per_row);
+        for lines in &self.data.lines
         {
-            'outer_loop: loop {
-                let byte = u8::from_str_radix(&self.data[char_index..char_index+2], 16).unwrap();
+            for span in &lines.0
+            {
+                let mut hex = span.content.to_string();
+                hex.retain(|c| c.is_whitespace() == false);
+                let byte = u8::from_str_radix(&hex, 16).unwrap();
                 output.push(byte);
-                char_index += 2;
-                while self.data.as_bytes()[char_index].is_ascii_whitespace()
-                {
-                    char_index += 1;
-                    if char_index >= self.data.len()
-                    {
-                        break 'outer_loop;
-                    }
-                }
             }
         }
         output
     }
 
-    pub fn save_data(&mut self)
+    fn save_data(&mut self)
     {
         self.output = "Converting data...".to_string();
         let data = self.data_to_vec_u8();
@@ -280,7 +324,7 @@ impl App
         self.output = format!("Saved to {}", self.path.to_str().unwrap());
     }
 
-    pub fn handle_event(&mut self, event: event::Event) -> Result<(),Box<dyn std::error::Error>>
+    fn handle_event(&mut self, event: event::Event) -> Result<(),Box<dyn std::error::Error>>
     {
         match event
         {
@@ -371,13 +415,13 @@ impl App
                 let text_view_rect = Rect::new(address_rect.width + hex_editor_rect.width, 0, (self.block_size * 2 * self.blocks_per_row + self.blocks_per_row) as u16, f.size().height - output_rect.height);
                 let output_block = tui::widgets::Paragraph::new(Text::raw(&self.output))
                     .block(Block::default().borders(tui::widgets::Borders::LEFT));
-                let address_block = tui::widgets::Paragraph::new(Text::raw(&self.address_view))
+                let address_block = tui::widgets::Paragraph::new(self.address_view.clone())
                     .block(Block::default().title("Address").borders(tui::widgets::Borders::ALL))
                     .scroll((self.scroll, 0));
-                let hex_editor_block = tui::widgets::Paragraph::new(Text::raw(&self.data))
+                let hex_editor_block = tui::widgets::Paragraph::new(self.data.clone())
                     .block(Block::default().title("Hex Editor").borders(tui::widgets::Borders::ALL))
                     .scroll((self.scroll, 0));
-                let text_view_block = tui::widgets::Paragraph::new(Text::raw(&self.text_view))
+                let text_view_block = tui::widgets::Paragraph::new(self.text_view.clone())
                     .block(Block::default().title("Text View").borders(tui::widgets::Borders::ALL))
                     .scroll((self.scroll, 0));
                 f.render_widget(output_block, output_rect);
