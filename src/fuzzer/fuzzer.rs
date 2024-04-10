844600015e76dfe0d5741a0aa4a155b22a81ec3c
Ettore Ricci <minecraft.etto@gmail.com>
2024-04-10 12:35:17 +0200

    moved fuzzer test from doctest to unit test because it was slow (0.77s)

--- src/fuzzer/fuzzer.rs
+++ src/fuzzer/fuzzer.rs
@@ -16,20 +16,26 @@ impl Fuzzer
     }
 
     /// Returns a sorted list of keys, from most relevant to least relevant.
-    /// ```
-    /// use hex_patch::fuzzer::fuzzer::Fuzzer;
-    /// 
-    /// let fuzzer = Fuzzer::new(&["cherry", "banana", "apple"]);
-    /// let results = fuzzer.fuzzy_search_sorted("a");
-    /// assert_eq!(results, vec!["apple", "banana", "cherry"]);
-    /// 
-    /// let results = fuzzer.fuzzy_search_sorted("an");
-    /// assert_eq!(results, vec!["banana", "apple", "cherry"]);
-    /// ```
     pub fn fuzzy_search_sorted(&self, key: &str) -> Vec<String>
     {
         let mut ret = self.entries.clone();
         ret.sort_by_key(|entry| -entry.score(key));
         ret.into_iter().map(|entry| entry.key).collect()
+    }
+}
+
+#[cfg(test)]
+mod test
+{
+    use super::*;
+
+    #[test]
+    pub fn test_fuzzer()
+    {
+        let fuzzer = Fuzzer::new(&["cherry", "banana", "apple"]);
+        let results = fuzzer.fuzzy_search_sorted("a");
+        assert_eq!(results, vec!["apple", "banana", "cherry"]);
+        let results = fuzzer.fuzzy_search_sorted("an");
+        assert_eq!(results, vec!["banana", "apple", "cherry"]);
     }
 }
