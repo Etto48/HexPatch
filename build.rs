498c749def961cccd1b64d5dadbe0c27a7c88b20
Ettore Ricci <minecraft.etto@gmail.com>
2024-05-06 00:21:48 +0200

    added executable icon for windows

---
+++ build.rs
@@ -0,0 +1,13 @@
+#[cfg(windows)]
+extern crate winres;
+
+#[cfg(windows)]
+fn main()
+{
+    let mut res = winres::WindowsResource::new();
+    res.set_icon("assets/logo.ico");
+    res.compile().unwrap();
+}
+
+#[cfg(unix)]
+fn main() {}
